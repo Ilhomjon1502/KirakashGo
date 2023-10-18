db14d0d9c3c4275be829d42463736b6724034422
Saydullo Abdurashidov <abdurashidov.developer@gmail.com>
2023-10-18 09:43:00 +0300

    5. AccountFragment

--- app/src/main/java/uz/ilhomjon/kirakashgo/screens/account/AccountFragment.kt
+++ app/src/main/java/uz/ilhomjon/kirakashgo/screens/account/AccountFragment.kt
@@ -17,7 +17,8 @@ private const val ARG_PARAM2 = "param2"
  * Use the [AccountFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class AccountFragment : Fragment() {
+class
+AccountFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
