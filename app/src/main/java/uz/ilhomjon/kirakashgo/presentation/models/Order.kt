2b2634fb377271b29c022141e942451bec78080f
Saydullo Abdurashidov <abdurashidov.developer@gmail.com>
2023-10-19 14:20:42 +0300

    9. OrderAdapter

---
+++ app/src/main/java/uz/ilhomjon/kirakashgo/presentation/models/Order.kt
@@ -0,0 +1,7 @@
+package uz.ilhomjon.kirakashgo.presentation.models
+
+data class Order(
+    var address:String,
+    var price:String,
+    var date:String?
+)
---
+++ app/src/main/java/uz/ilhomjon/kirakashgo/presentation/screens/orders/OrderAdapter.kt
@@ -0,0 +1,55 @@
+package uz.ilhomjon.kirakashgo.presentation.screens.orders
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import uz.ilhomjon.kirakashgo.databinding.RvDateItemBinding
+import uz.ilhomjon.kirakashgo.databinding.RvOrderItemBinding
+import uz.ilhomjon.kirakashgo.presentation.models.Order
+
+class OrderAdapter(val list: ArrayList<Order>) : RecyclerView.Adapter<ViewHolder>() {
+
+    inner class DateVh(val rvDateItem: RvDateItemBinding) : ViewHolder(rvDateItem.root) {
+        fun onBind(date: String) {
+            rvDateItem.dateTv.text = date
+        }
+    }
+
+    inner class OrderVh(val rvOrderItem: RvOrderItemBinding) : ViewHolder(rvOrderItem.root) {
+        fun onBind(order: Order) {
+            rvOrderItem.addressTv.text = order.address
+            rvOrderItem.price.text = "Summa: ${order.price}"
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return if (viewType == 1) DateVh(
+            RvDateItemBinding.inflate(
+                LayoutInflater.from(parent.context), parent, false
+            )
+        ) else OrderVh(
+            RvOrderItemBinding.inflate(
+                LayoutInflater.from(parent.context), parent, false
+            )
+        )
+    }
+
+    override fun getItemCount(): Int = list.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        if (getItemViewType(position) == 1) {
+            val dateVh = holder as DateVh
+            dateVh.onBind(list[position].date ?: "")
+        } else {
+            val orderVh = holder as OrderVh
+            orderVh.onBind(list[position])
+        }
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return if (list[position].date != null) 1 else 2
+
+    }
+}
