2d452d03bfe0a2bbeac7c3dc3d35407216164402
newuser <ilhomjonibragimov1502@gmail.com>
2023-11-23 15:11:06 +0500

    add location service and change homeFragment

--- app/src/main/java/uz/ilhomjon/kirakashgo/presentation/screens/home/HomeFragment.kt
+++ app/src/main/java/uz/ilhomjon/kirakashgo/presentation/screens/home/HomeFragment.kt
@@ -1,6 +1,8 @@
 package uz.ilhomjon.kirakashgo.presentation.screens.home
 
+import android.Manifest
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -8,13 +10,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.animation.AnimationUtils
+import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat
 import androidx.core.os.bundleOf
 import androidx.core.view.GestureDetectorCompat
 import androidx.navigation.NavOptions
 import androidx.navigation.fragment.findNavController
+import com.github.florent37.runtimepermission.kotlin.askPermission
 import uz.ilhomjon.kirakashgo.R
 import uz.ilhomjon.kirakashgo.databinding.FragmentHomeBinding
 import uz.ilhomjon.kirakashgo.presentation.common.MyGestureListener
+import uz.ilhomjon.kirakashgo.taximetr.MyLocationService
 
 class HomeFragment : Fragment() {
 
@@ -38,7 +44,7 @@ class HomeFragment : Fragment() {
             findNavController().navigate(R.id.driverFragment, bundleOf("1" to 1), navOption.build())
         }
 
-
+        locationService()
         return binding.root
     }
 
@@ -69,5 +75,33 @@ class HomeFragment : Fragment() {
     private fun pxToDp(px: Int): Float {
         val density = resources.displayMetrics.density
         return px / density
+    }
+
+    fun locationService(){
+        askPermission(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_BACKGROUND_LOCATION, Manifest.permission.FOREGROUND_SERVICE){
+            //all permissions already granted or just granted
+            ContextCompat.startForegroundService(requireContext(), Intent(requireActivity(), MyLocationService::class.java))
+        }.onDeclined { e ->
+            if (e.hasDenied()) {
+
+                AlertDialog.Builder(requireContext())
+                    .setMessage("Please accept our permissions")
+                    .setPositiveButton("yes") { dialog, which ->
+                        e.askAgain();
+                    } //ask again
+                    .setNegativeButton("no") { dialog, which ->
+                        dialog.dismiss();
+                    }
+                    .show();
+            }
+
+            if(e.hasForeverDenied()) {
+                //the list of forever denied permissions, user has check 'never ask again'
+
+                // you need to open setting manually if you really need it
+                e.goToSettings();
+            }
+        }
+
     }
 }
